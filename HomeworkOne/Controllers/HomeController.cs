f24a74f40224d0c99b001c99bb7faeb0c4f9d4bb
kesenalican <46851204+kesenalican@users.noreply.github.com>
2022-03-05 14:55:56 +0300

    first homework done

---
+++ HomeworkOne/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using HomeworkOne.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeworkOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(InfoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Failed();
+            }
+            return Success();
+
+        }
+
+        public IActionResult Failed()
+        {
+            return BadRequest(new ResponseViewModel { Data = "null", Error = "Hatalı Giriş" , Success=false});
+        }
+
+        public IActionResult Success()
+        {
+            return Ok(new ResponseViewModel { Data = "Giriş Başarılı", Success = true, Error = "null" });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
---
+++ HomeworkOne/Models/ResponseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeworkOne.Models
+{
+    public class ResponseViewModel
+    {
+        public bool Success { get; set; }
+
+        public String Error { get; set; }
+
+        public String Data { get; set; }
+    }
+}
